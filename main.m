7ed0423a5b2811202727f38bd9bb17bd9aa33ae3
mturkoglu23 <54291142+mturkoglu23@users.noreply.github.com>
2021-10-16 00:14:49 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,21 +1,12 @@
 clear;clc;
-% % layer ='fc1000';
-% % net = densenet201;
 net=alexnet;
 layer='fc6';
-% % net1 = resnet101;
-% % net2 = resnet101;
-% % net3=efficientnetb0;
-% % layer1='efficientnet-b0|model|head|dense|MatMul';
-% % net3=inceptionresnetv2;
-% % layer1='predictions';
-% 
-dataFolder ="E:\New Studies\tevfik çalışma\uygulamalar\uygulama\dataset\hockey";
+
+dataFolder ="...\Movie dataset";
 [files,labels] = hmdb51Files(dataFolder);
 
-uzunluk=length(files)
+uzunluk=length(files);
 for idx=1:uzunluk
-    idx
 filename = files(idx);
 video = VideoReader(filename);
 vidwidth=video.Width;
@@ -29,26 +20,23 @@ k=k+1;
 end
 
 kk=length(img);
-tt=ceil(kk/5);
-% [rr]=randperm(kk,5);
+tt=ceil(kk/10);
 
 m=1;
 for i=1:tt:length(img)
     xxx=img{i};
     img1=imresize(xxx,[227 227]);
     features(:,m) = activations(net,img1,layer);
-%     features1(:,m) = activations(net1,img1,layer);
     m=m+1;
 end
 
-feat_den{idx,1}=features;
-% feat_res{idx,1}=features1;
+feat_alex{idx,1}=features;
 
 end
 
-%%
-for ii=1:1000
-      aa=feat_den{ii};
+%% feature average pooling approach
+for ii=1:200
+      aa=feat_alex{ii};
       for jj=1:4096
       xx(jj,1)=mean(abs(aa(jj,:)));
       end
@@ -57,9 +45,9 @@ end
 label=double(labels);
 ff1=[feat',label];
 
-%%
-for ii=1:1000
-      aa=feat_den{ii};
+%% feature standard pooling approach
+for ii=1:200
+      aa=feat_alex{ii};
       for jj=1:4096
       xx(jj,1)=std(abs(aa(jj,:)));
       end
@@ -68,10 +56,13 @@ end
 label=double(labels);
 ff2=[feat',label];
 
-%%
-for ii=1:1000
-      aa=feat_den{ii};
+%% feature fusion approach
+for ii=1:200
+      aa=feat_alex{ii};
       xx1=reshape(aa,[],1);
 fusion(:,ii)=xx1;
 end
 ff3=[fusion',label];
+
+%% fc+relu+fc+softmax (classification process)
+
